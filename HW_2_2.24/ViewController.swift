5c894530c67a01f646aca151e297f2d70d03ceff
Art <artem_chernenko@ro.ru>
2021-12-10 09:47:41 +0300

    Initial Commit

---
+++ HW_2_2.24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2_2.24
+//
+//  Created by Артем Черненко on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
